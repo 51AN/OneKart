25c8c1fadb564765634187f41307cd912442bf0b
51AN <sianashsad@iut-dhaka.edu>
2023-03-24 20:58:13 +0600

    Loading screen bullshit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,35 @@ import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
+import { HashLoader } from 'react-spinners'
+import { useState, useEffect } from 'react'
+import "../src/App.css"
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+    },[])
+
   return (
-    <Router>
-      <div>
-        <Navbar/ >
-        <Routes>
-          <Route exact path='/' Component={Home} />
-          <Route exact path='/login' Component={Login} />
-          <Route exact path='/signup' Component={Signup} />
-        </Routes>
-      </div>
-    </Router>
+  <>
+    {loading?<div className="loadingScreen"><HashLoader color={"#FA6D4F"} loading={loading} size={100} aria-label="Loading Spinner" data-testid="loader"/> </div> 
+    :<><Router>
+    <div>
+      <Navbar/ >
+      <Routes>
+        <Route exact path='/' Component={Home} />
+        <Route exact path='/login' Component={Login} />
+        <Route exact path='/signup' Component={Signup} />
+      </Routes>
+    </div>
+  </Router> </>}
+    
+  </>
   )
 }
 
